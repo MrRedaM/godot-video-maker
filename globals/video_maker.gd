50729097cb926f7f7f8c14beda6156479f5cd566
Reda MORSLI <47378462+MrRedaM@users.noreply.github.com>
2023-10-03 11:11:17 -0400

    Added TranslationPath2DEvent

--- globals/video_maker.gd
+++ globals/video_maker.gd
@@ -5,9 +5,10 @@ func _search_node(node_path):
 	return get_tree().current_scene.find_child(node_path)
 
 
-func _animate_canvas_property(event: Event, canvas: CanvasItem, property: String, value):
+func _animate_canvas_property(event: Event, canvas: CanvasItem, property: String, value) -> Tween:
 	var tween = canvas.create_tween()
 	tween.set_trans(event.trans_type)
 	tween.set_ease(event.ease_type)
 	tween.tween_property(canvas, property, value, event.duration)
+	return tween
 
