ba5edf6ca187902352280d5cc937dc9eafde80f0
Reda MORSLI <47378462+MrRedaM@users.noreply.github.com>
2023-10-20 11:41:42 -0400

    Change Video to MediaAnimation

--- addons/video-maker/events/tween_event/tweeners/scale_tweener.gd
+++ addons/video-maker/events/tween_event/tweeners/scale_tweener.gd
@@ -8,14 +8,14 @@ extends CustomPropertyTweener
 
 func init(sequence: Sequence, target_nodes):
 	super(sequence, target_nodes)
-	if hide_on_sequence_start:
-		for node in target_nodes:
-			if node is CanvasItem:
-				if node.get_parent() is Container:
-					await _sequence.get_tree().process_frame
+	for node in target_nodes:
+		if node is CanvasItem:
+			if node.get_parent() is Container:
+				await _sequence.get_tree().process_frame
+			if hide_on_sequence_start and node is CanvasItem:
 				node.scale = Vector2.ZERO
-				if node is Control:
-					node.pivot_offset = node.size / 2
+			if node is Control:
+				node.pivot_offset = node.size / 2
 
 
 func get_property() -> String:
--- addons/video-maker/plugin.gd
+++ addons/video-maker/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("Video", "Node", preload("res://addons/video-maker/entities/video.gd"), preload("res://addons/video-maker/entities/ic_video.svg"))
+	add_custom_type("MediaAnimation", "Node", preload("res://addons/video-maker/entities/media_animation.gd"), preload("res://addons/video-maker/entities/ic_video.svg"))
 	add_custom_type("Sequence", "Node", preload("res://addons/video-maker/entities/sequence.gd"), preload("res://addons/video-maker/entities/ic_sequence.svg"))
 
 
